a384a3b52515762c3dfce8ab2cbe8e75ce9c1673
Chiến Trần <lonelyheart0309@gmail.com>
2019-04-29 11:21:47 +0700

    search Candidate

--- DoAnTotNghiep/src/main/java/com/candidate/controller/TestController.java
+++ DoAnTotNghiep/src/main/java/com/candidate/controller/TestController.java
@@ -31,20 +31,30 @@ import com.position.dao.PositionDAO;
 
 @Controller
 public class TestController {
+	//cái này để khỏi tạo new CandidateDAO
 	@Autowired
 	private CandidateDAO candidateDAO ;
 	@Autowired
 	private PositionDAO positionDAO ;
-	//Candidate can = new Candidate();
 	
+	PositionDAO posDAO = new PositionDAO();
+	
+
 	@GetMapping("/index")
 	public String index(Model model) {
+
 		ArrayList<Candidate> ds_Can = new ArrayList<>();
 		ds_Can = candidateDAO.getAllCandidate_M();
 		model.addAttribute("dsCandidates", ds_Can);
+
+		//get all Positionn để đổ name vô select box
+		ArrayList<Position> arrPos = new ArrayList<>();	
+		arrPos = posDAO.getAllPosition();
+		model.addAttribute("listPosition", arrPos);
+		
 		return "index";
 	}
-	
+
 	@PostMapping("/index/setInterview")
 	public @ResponseBody String setInterview(HttpServletRequest request) throws JSONException {
 		//Lấy dữ liệu từ ajax gửi về
@@ -54,81 +64,81 @@ public class TestController {
 		String venue = request.getParameter("venue");
 		String note = request.getParameter("note");
 		JSONArray arr = new JSONArray(request.getParameter("id_cmnd"));
-		
+
 		//Thêm interview
 		for (int i = 0; i < arr.length(); i++) {
-            String idCan = arr.getJSONObject(i).getString("idCan");
-            //System.out.println(idCan);
-            String cmnd = arr.getJSONObject(i).getString("cmnd");
-            //System.out.println(cmnd);
-            
-            String stus = "Wait for interview";
-            
+			String idCan = arr.getJSONObject(i).getString("idCan");
+			//System.out.println(idCan);
+			String cmnd = arr.getJSONObject(i).getString("cmnd");
+			//System.out.println(cmnd);
+
+			String stus = "Wait for interview";
+
 			Candidate can = new Candidate();
 			can = candidateDAO.getCandidateById(idCan);
 			if(can.getInterview()==null) {
 				ArrayList<Object> rounds = new ArrayList<>();
 				Map<String, Object> rnd = new HashMap<String, Object>();
-	    		rnd.put("idRound",rounds.size()+1+"");
-	    		rnd.put("interviewer",interviewer);
-	    		rnd.put("date", date);
-	    		rnd.put("time", time);
-	    		rnd.put("venue", venue);
-	    		rnd.put("note", note);
-	    		rnd.put("result", "Unknown");	
-	    		rounds.add(rnd);
-	    		
-	    		candidateDAO.addInterview(idCan, cmnd, rounds, stus);
+				rnd.put("idRound",rounds.size()+1+"");
+				rnd.put("interviewer",interviewer);
+				rnd.put("date", date);
+				rnd.put("time", time);
+				rnd.put("venue", venue);
+				rnd.put("note", note);
+				rnd.put("result", "Unknown");	
+				rounds.add(rnd);
+
+				candidateDAO.addInterview(idCan, cmnd, rounds, stus);
 			}else {
 				if(!(can.getInterview().get("finalResult")).equals("Unknown")) {
 					stus = "Interviewing";
 				}
 				ArrayList<Object> rounds=  (ArrayList<Object>) can.getInterview().get("rounds");
-	            Map<String, Object> rnd = new HashMap<String, Object>();
-	    		rnd.put("idRound",rounds.size()+1+"");
-	    		rnd.put("interviewer",interviewer);
-	    		rnd.put("date", date);
-	    		rnd.put("time", time);
-	    		rnd.put("venue", venue);
-	    		rnd.put("note", note);
-	    		rnd.put("result", "Unknown");	
-	    		rounds.add(rnd);
-	    		
-	    		candidateDAO.addInterview(idCan, cmnd, rounds,stus);
+				Map<String, Object> rnd = new HashMap<String, Object>();
+				rnd.put("idRound",rounds.size()+1+"");
+				rnd.put("interviewer",interviewer);
+				rnd.put("date", date);
+				rnd.put("time", time);
+				rnd.put("venue", venue);
+				rnd.put("note", note);
+				rnd.put("result", "Unknown");	
+				rounds.add(rnd);
+
+				candidateDAO.addInterview(idCan, cmnd, rounds,stus);
 			}			
-        }
-			
+		}
+
 		return "Set Interview Success";
 	}
-	
+
 	@PostMapping("/index/deleteCandidate")
 	public @ResponseBody String deleteCan(HttpServletRequest request) throws JSONException{
 		JSONArray arr = new JSONArray(request.getParameter("id_cmnd"));
 		for (int i = 0; i < arr.length(); i++) {
 			String idCan = arr.getJSONObject(i).getString("idCan");
-            String cmnd = arr.getJSONObject(i).getString("cmnd");
-            
-            candidateDAO.deleteCandidate(idCan, cmnd);
+			String cmnd = arr.getJSONObject(i).getString("cmnd");
+
+			candidateDAO.deleteCandidate(idCan, cmnd);
 		}
 		return "Delete Candidates Success";		
 	}
-	
-//	@PostMapping("/candidate")
-//	public String can(HttpServletRequest request, RedirectAttributes attr ) {
-//		String id = request.getParameter("id");
-//		//can = candidateDAO.getCandidateById(id);
-//		
-//		attr.addAttribute("IdCan", id);
-//		return "redirect:/login";
-//	}
-	
+
+	//	@PostMapping("/candidate")
+	//	public String can(HttpServletRequest request, RedirectAttributes attr ) {
+	//		String id = request.getParameter("id");
+	//		//can = candidateDAO.getCandidateById(id);
+	//		
+	//		attr.addAttribute("IdCan", id);
+	//		return "redirect:/login";
+	//	}
+
 	@GetMapping("/profile")
 	public String profile(Model model, @RequestParam("id") String id){
 		Candidate can = candidateDAO.getCandidateById(id);
 		if(!(can.getIdCan()==null)) {
 			System.out.println(can);
 			model.addAttribute("candidate",can);
-			
+
 			//Đổ dữ liệu cho status
 			String [] n = {"New","Reject"};
 			String[] in = {"Interviewing","Interview Pass","Interview Fail"};
@@ -143,8 +153,8 @@ public class TestController {
 			}else {
 				model.addAttribute("stt",p);
 			}
-			
-			
+
+
 			//Đổ dữ liệu cho Position Apply
 			ArrayList<String> dsNamePos = new ArrayList<>();
 			ArrayList<Position> pos = new ArrayList<Position>();
@@ -153,20 +163,20 @@ public class TestController {
 				dsNamePos.add(pos.get(i).getName());
 			}
 			model.addAttribute("dsNamePos", dsNamePos);
-			
-			
+
+
 			//Đổ dữ liệu cho tag Interview
 			int pass = 0;
 			int fail = 0;
 			int totalRound = 0;
 			String isIn = "Not";
 			String[] fr = {"Unknown", "Pass","Fail"};
-			
+
 			if(can.getInterview()!=null) {
 				isIn = (String) can.getInterview().get("finalResult");
 				ArrayList<Object> rounds=  (ArrayList<Object>) can.getInterview().get("rounds");
 				totalRound = rounds.size();
-				
+
 				ArrayList<String> dsR = new ArrayList<>();
 				for(int i = 0; i<rounds.size(); i++) {
 					Map<String, Object> rnd = new HashMap<String, Object>();
@@ -186,12 +196,12 @@ public class TestController {
 			model.addAttribute("Fail", fail);
 			model.addAttribute("fr", fr);
 			model.addAttribute("isIn",isIn);
-			
+
 			return "profile";
 		}
 		return null;
 	}
-	
+
 	@PostMapping("/profile/editProfile")
 	public @ResponseBody String editProfile(HttpServletRequest req) {
 		String id = req.getParameter("id");
@@ -218,7 +228,7 @@ public class TestController {
 		candidateDAO.editProfile(can);
 		return "Edit Success";
 	}
-	
+
 	@PostMapping("/profile/addRoundInterview")
 	public @ResponseBody String addRound(HttpServletRequest request) throws JSONException {
 		//Lấy dữ liệu từ ajax gửi về
@@ -230,22 +240,22 @@ public class TestController {
 		String venue = request.getParameter("venue");
 		String note = request.getParameter("note");
 		String result = request.getParameter("result");
-		
+
 		String stus = "Wait for interview";
-        
+
 		if(can.getInterview()==null) {
 			ArrayList<Object> rounds = new ArrayList<>();
 			Map<String, Object> rnd = new HashMap<String, Object>();
-    		rnd.put("idRound",rounds.size()+1+"");
-    		rnd.put("interviewer",interviewer);
-    		rnd.put("date", date);
-    		rnd.put("time", time);
-    		rnd.put("venue", venue);
-    		rnd.put("note", note);
-    		rnd.put("result", result);	
-    		rounds.add(rnd);
-    		
-    		candidateDAO.addInterview(can.getIdCan(), can.getCmnd(), rounds, stus);
+			rnd.put("idRound",rounds.size()+1+"");
+			rnd.put("interviewer",interviewer);
+			rnd.put("date", date);
+			rnd.put("time", time);
+			rnd.put("venue", venue);
+			rnd.put("note", note);
+			rnd.put("result", result);	
+			rounds.add(rnd);
+
+			candidateDAO.addInterview(can.getIdCan(), can.getCmnd(), rounds, stus);
 		}else {
 			ArrayList<Object> rounds=  (ArrayList<Object>) can.getInterview().get("rounds");
 			for(int i = 0 ; i<rounds.size();i++) {
@@ -254,22 +264,22 @@ public class TestController {
 					stus = "Interviewing";
 				}
 			}
-            Map<String, Object> rnd = new HashMap<String, Object>();
-    		rnd.put("idRound",rounds.size()+1+"");
-    		rnd.put("interviewer",interviewer);
-    		rnd.put("date", date);
-    		rnd.put("time", time);
-    		rnd.put("venue", venue);
-    		rnd.put("note", note);
-    		rnd.put("result", result);	
-    		rounds.add(rnd);
-    		
-    		candidateDAO.addInterview(can.getIdCan(), can.getCmnd(), rounds,stus);
+			Map<String, Object> rnd = new HashMap<String, Object>();
+			rnd.put("idRound",rounds.size()+1+"");
+			rnd.put("interviewer",interviewer);
+			rnd.put("date", date);
+			rnd.put("time", time);
+			rnd.put("venue", venue);
+			rnd.put("note", note);
+			rnd.put("result", result);	
+			rounds.add(rnd);
+
+			candidateDAO.addInterview(can.getIdCan(), can.getCmnd(), rounds,stus);
 		}			
-			
+
 		return "Add Round Success";
 	}
-	
+
 	@PostMapping("/profile/selectRound")
 	public ResponseEntity<?> showRound(@RequestBody FormRound data) {
 		String idRound = data.getRound();
@@ -281,7 +291,7 @@ public class TestController {
 		}
 		return null;
 	}
-	
+
 	@PostMapping("/profile/editRoundInterview")
 	public @ResponseBody String editRound(HttpServletRequest request) throws JSONException {
 		//Lấy dữ liệu từ ajax gửi về
@@ -302,7 +312,7 @@ public class TestController {
 				rounds.remove(i);
 				System.out.println("lala");
 			}
-			
+
 		}	
 		Map<String, Object> rnd = new HashMap<String, Object>();
 		rnd.put("idRound",idr);
@@ -313,11 +323,11 @@ public class TestController {
 		rnd.put("note", note);
 		rnd.put("result", result);	
 		rounds.add(rnd);
-		
+
 		candidateDAO.editRoundById(rounds,can.getIdCan(), can.getCmnd());
 		return "Edit Round Success";
 	}
-	
+
 	@PostMapping("/profile/finishInterview")
 	public @ResponseBody String updateFinalResultInterview(HttpServletRequest req) {
 		String idCan = req.getParameter("idCan");
