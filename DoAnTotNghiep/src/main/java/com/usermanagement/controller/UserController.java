12eff470c1ee9a7f292859040bd1fc4fed20ef0a
dckool <email@example.com>
2019-05-13 11:36:15 +0700

    thêm các chức năng user, cơ bản đủ báo caos, chưa vadidate vs imageeee

--- DoAnTotNghiep/src/main/java/com/usermanagement/controller/UserController.java
+++ DoAnTotNghiep/src/main/java/com/usermanagement/controller/UserController.java
@@ -1,14 +1,70 @@
 package com.usermanagement.controller;
 
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.entity.User;
+import com.usermanagement.dao.UserDAO;
 
 @Controller
 public class UserController {
 	
+	UserDAO dao = new UserDAO();
+	
 	@GetMapping("/userMgmt")
-	public String userMgmt() {
+	public String userMgmt(Model model) {
+		ArrayList<User> arrU = new ArrayList<>();
+		arrU = dao.getAllUser();
+		model.addAttribute("listUser",arrU);
 		return "userManagement";
+	}
+	
+	@RequestMapping(value="/deleteUser",method=RequestMethod.POST)
+	public  @ResponseBody void  deleteUser( HttpServletRequest request) {
+		
+		String id = request.getParameter("id");
+		String tk = request.getParameter("tk");
+		dao.deleteUser(id, tk);
+		System.out.println("delete success");
+	}
+	
+	@RequestMapping(value="/addUser",method=RequestMethod.POST)
+	public  @ResponseBody void  addNewUser( HttpServletRequest request) {
+		
+		//lấy mảng Pos để tạo id mới
+		ArrayList<User> arrPos = new ArrayList<>();
+		arrPos = dao.getAllUser();
+		String id_user = dao.autoId(arrPos);
+		
+		String name = request.getParameter("name");
+		String code = request.getParameter("role");
+		String pass = request.getParameter("pass");
+		String tk = request.getParameter("tk");
+		String avatar = "https://docs.google.com/uc?id=1zTOyl_cNXdD5MIADpl75Dmr3tKrBy1Gd";
+		User u = new User(id_user, tk, code, name, pass, avatar);
+		dao.addNewUser(u);
+		System.out.println("Add success user: "+u.getTk());
+	}
+	
+	@RequestMapping(value="/updateUser",method=RequestMethod.POST)
+	public  @ResponseBody void update( HttpServletRequest request) {
+		
+		String name = request.getParameter("name");
+		String code = request.getParameter("role");
+		String id = request.getParameter("id");
+		String tk = request.getParameter("tk");
+		String avatar = "https://docs.google.com/uc?id=1zTOyl_cNXdD5MIADpl75Dmr3tKrBy1Gd";
+		
+		dao.updateUser(id, tk, name, code, avatar);
+		System.out.println("update success");
 	}
 
 }
--- DoAnTotNghiep/src/main/java/com/usermanagement/dao/UserDAO.java
+++ DoAnTotNghiep/src/main/java/com/usermanagement/dao/UserDAO.java
@@ -1,5 +1,137 @@
 package com.usermanagement.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PrimaryKey;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.UpdateItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.spec.DeleteItemSpec;
+import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ReturnValue;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
+import com.entity.Position;
+import com.entity.Questions;
+import com.entity.User;
+
+@Repository
 public class UserDAO {
+	
+	AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+			.withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-west-2"))
+			.build();
+
+	DynamoDB dynamoDB = new DynamoDB(client);
+
+
+	public ArrayList<User> getAllUser () {
+		ArrayList<User> ds = new ArrayList<User>();
+		ScanResult rs = null;
+		do {
+			ScanRequest req = new ScanRequest();
+			req.withTableName("User");
+			if(rs != null){
+				req.setExclusiveStartKey(rs.getLastEvaluatedKey());
+			}
+			rs = client.scan(req);
+			List<Map<String, AttributeValue>> rows = rs.getItems();
+
+			for(Map<String, AttributeValue> map : rows){
+				try{
+					String id_user = map.get("id_user").getS();
+					String tk = map.get("tk").getS();
+					String code= map.get("code").getS();
+					String name= map.get("name_user").getS();
+					String pass= map.get("pass").getS();
+					String avatar= map.get("avatar").getS();
+					User u = new User(id_user, tk, code, name, pass, avatar);
+					ds.add(u);
+				} catch (NumberFormatException e){
+					System.out.println(e.getMessage());
+				}
+			}
+
+		}while(rs.getLastEvaluatedKey() != null);
+		return ds;
+	}
+	
+	public void addNewUser(User q) {
+
+		Table table = dynamoDB.getTable("User");
+
+		try {
+			System.out.println("Adding a new item...");
+			table.putItem(new Item().withPrimaryKey("id_user", q.getId_user(), "tk", q.getTk())
+							.withString("name_user",q.getName())
+	                		.withString("pass",q.getPass())
+	                		.withString("code",q.getCode())
+	                		.withString("avatar",q.getAvatar()));
+
+			System.out.println("PutItem succeeded:" + q);
+
+		}
+		catch (Exception e) {
+			System.err.println("Unable to add item: " + q.getName());
+			System.err.println(e.getMessage());
+		}
+
+	}
+	
+	public void updateUser(String id, String tk, String name, String code, String avatar) {
+
+		Table table = dynamoDB.getTable("User");
+
+		UpdateItemSpec updateItemSpec = new UpdateItemSpec().withPrimaryKey("id_user", id, "tk", tk)
+				.withUpdateExpression("set name_user = :a, code = :b, avatar = :c")
+				.withValueMap(new ValueMap().withString(":a", name)
+						.withString(":b", code)
+						.withString(":c", avatar))
+				.withReturnValues(ReturnValue.UPDATED_NEW);
+
+		try {
+			System.out.println("Updating the item...");
+			UpdateItemOutcome outcome = table.updateItem(updateItemSpec);
+			System.out.println("UpdateItem succeeded:\n" + outcome.getItem().toJSONPretty());
+
+		}
+		catch (Exception e) {
+			System.err.println("Unable to update item: " + tk);
+			System.err.println(e.getMessage());
+		}
+	}
+	
+	public void deleteUser(String id_user, String tk) {
+		Table table = dynamoDB.getTable("User");
+		DeleteItemSpec deleteItemSpec = new DeleteItemSpec()
+	            .withPrimaryKey(new PrimaryKey("id_user", id_user, "tk", tk));
+
+	        // Conditional delete (we expect this to fail)
+
+	        try {
+	            System.out.println("Attempting a conditional delete...");
+	            table.deleteItem(deleteItemSpec);
+	            System.out.println("DeleteItem succeeded");
+	        }
+	        catch (Exception e) {
+	            System.err.println(e.getMessage());
+	        }
+	}
+	
+	//tự tăng id Pos
+		public String autoId(ArrayList<User> arr) {
+			int count = arr.size()+1;
+			return "user_"+count+"";
+		}
 
 }
